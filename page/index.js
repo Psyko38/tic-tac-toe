896f580a3e8b0238bee8f2e1a0fc4455cebbfbbe
Psyko38 <110417117+Psyko38@users.noreply.github.com>
2025-01-31 16:05:40 +0100

    feat app : new js fix

--- page/index.js
+++ page/index.js
@@ -18,9 +18,16 @@ for (let c of btn) {
 					img.alt = "X";
 					turn = 1;
 				}
+				let win = ChekAllTable();
+				if (win == "X") {
+					document.querySelector(".bg").style.display = "flex";
+					document.querySelector(".pupuX").style.display = "flex";
+				} else if (win == "O") {
+					document.querySelector(".bg").style.display = "flex";
+					document.querySelector(".pupuO").style.display = "flex";
+				}
 			}
 		}
-		GetTable();
 	});
 }
 
@@ -31,5 +38,57 @@ function GetTable() {
 	for (let c of btn) {
 		table.push(c.querySelector("img").alt);
 	}
-	console.log(table);
+}
+
+function ChekTable(space, index) {
+	let car = table[index];
+	let pass = 0;
+	let i = index;
+	for (let c = 0; c < 3; c++) {
+		if (table[i] == car) {
+			pass++;
+		}
+		i = i + space;
+	}
+	if (pass == 3) {
+		return car;
+	}
+}
+
+function ChekAllTable() {
+	GetTable();
+	let Result = "";
+
+	Result = ChekTable(1, 0);
+	if (Result == "X" || Result == "O") {
+		return Result;
+	}
+	Result = ChekTable(1, 3);
+	if (Result == "X" || Result == "O") {
+		return Result;
+	}
+	Result = ChekTable(1, 6);
+	if (Result == "X" || Result == "O") {
+		return Result;
+	}
+	Result = ChekTable(2, 2);
+	if (Result == "X" || Result == "O") {
+		return Result;
+	}
+	Result = ChekTable(3, 0);
+	if (Result == "X" || Result == "O") {
+		return Result;
+	}
+	Result = ChekTable(3, 1);
+	if (Result == "X" || Result == "O") {
+		return Result;
+	}
+	Result = ChekTable(3, 2);
+	if (Result == "X" || Result == "O") {
+		return Result;
+	}
+	Result = ChekTable(4, 0);
+	if (Result == "X" || Result == "O") {
+		return Result;
+	}
 }
